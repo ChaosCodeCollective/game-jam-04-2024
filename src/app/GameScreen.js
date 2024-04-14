ccaf1662d329272bad1017224efbb072719c8d45
Chris Patenaude <patenauc@oregonstate.edu>
2024-04-14 14:29:04 -0700

    Use single sprite png

--- src/app/GameScreen.js
+++ src/app/GameScreen.js
@@ -1,38 +1,1 @@
-'use client'
-
-import React, { useEffect, useRef } from "react";
-import * as PIXI from "pixi.js";
-import { Assets, Sprite } from 'pixi.js';
-import player from '../assets/sprites/characters/player.png';
-import playerData from '../assets/sprites/characters/player.json'
-import slime from '../assets/sprites/characters/slime.png';
-import slimeData from '../assets/sprites/characters/slime.json';
-export default function Snake() {
-  const container = useRef(null);
-  const app = useRef(null);
-
-  useEffect(() => {
-    (async () => {
-      app.current = new PIXI.Application();
-      await app.current.init({
-        width: 800,
-        height: 600,
-        backgroundColor: 0x1099bb,
-      });
-      container.current.appendChild(app.current.canvas);
-       // Load the bunny texture
-      const texture = await Assets.load(player);
-
-      // Create a bunny Sprite
-      const bunny = new Sprite(texture);
-      // Move the sprite to the center of the screen
-      bunny.x = app.current.screen.width / 2;
-      bunny.y = app.current.screen.height / 2;
-      app.current.stage.addChild(bunny);
-    })();
-  }); // Dependency on container.current to reinitialize if needed
-
-  return (
-    <div ref={container} className="grow m-5 rounded overflow-hidden"></div>
-  );
-}
+'use client'import React, { useEffect, useRef } from "react";import * as PIXI from "pixi.js";export default function Snake() {  const container = useRef(null);  const app = useRef(null);  useEffect(() => {    // Create a PIXI Application    async function setup() {      app.current = new PIXI.Application();      await app.current.init({        width: 800,        height: 600,        backgroundColor: 0x1099bb,      })      container.current.appendChild(app.current.canvas);      const texture = await PIXI.Assets.load("/assets/sprites/characters/player.png");      const frameCount = 6;      const frameWidth = 48;      const frameHeight = 48;      const frames = [];      for (let i = 0; i < frameCount; i++) {        const frame = new PIXI.Rectangle(i * frameWidth, 0, frameWidth, frameHeight);        frames.push(          new PIXI.Texture({            source: texture.source,            frame,          })        );      }      const animatedSprite = new PIXI.AnimatedSprite(frames);      animatedSprite.animationSpeed = 0.5;      animatedSprite.x = 100;      animatedSprite.y = 100;      animatedSprite.width = 48;      animatedSprite.height = 48;      animatedSprite.play();      app.current.stage.addChild(animatedSprite);    }    setup().then(() => console.log("setup complete"))    return () => {      // cleanup      app.current = null;    };  }, []);  return <div ref={container} className="grow m-5 rounded overflow-hidden"></div>;}
